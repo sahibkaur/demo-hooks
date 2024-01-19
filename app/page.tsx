e4f0222b5f57a227e02caa72f425d989c64fa3cd
Sahibpreet Kaur <sahibkaur81@gmail.com>
2024-01-19 16:54:28 +0530

    Demostrate useMemo hook

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import {useState} from "react";
 import Count from "./count";
 import Toggle from "./toggle";
+import Theme from './theme';
 
 export default function Home() {
 
@@ -16,6 +17,7 @@ const changeName = () => {
     <div>
       <Count></Count>
       <Toggle></Toggle>
+      <Theme></Theme>
       {/* <p>{name}</p>
       <button onClick={changeName}>Click Button</button> */}
     </div>
